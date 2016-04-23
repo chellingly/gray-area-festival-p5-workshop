5e799366a618274585940834d0d942d860263bc4
chellingly <chelleysherman@gmail.com>
2016-04-23 14:59:57 -0700

    workshop4 commit

---
+++ Workshop_4_animation/sketch.js
@@ -0,0 +1,23 @@
+var x = 0;
+var speed=4;
+function setup() {
+  createCanvas(500,500);
+  
+}
+
+function draw() {
+  
+  background(0);
+  fill(255);
+  ellipse(x,250,30,30);
+  
+  // if(x>width){
+  //   speed=-4;
+  // }else if(x<0){
+  //   speed=4;
+  // }
+  
+  x=x+speed;
+  
+  
+}
