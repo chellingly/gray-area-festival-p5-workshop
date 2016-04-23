0e14acca189893865d8b137d527001b002fd5fde
chellingly <chelleysherman@gmail.com>
2016-04-23 14:58:17 -0700

    workshop1 commit

---
+++ Workshop_1_function_example/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500,500);
+  background(0);
+  
+}
+
+function draw() {
+  fill(255);
+  ellipse(mouseX,mouseY,20,20);
+}
---
+++ Workshop_1_function_example/sketch/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(500,500);
+  background(0);
+}
+
+function draw() {
+  fill(255);
+  ellipse(mouseX,mouseY,30,30);
+}
